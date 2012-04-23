d97552ef1a597f58c97ac24bd5106258fdb43c90
landon d clark <landon.d.clark@gmail.com>
2012-04-23 16:58:14 -0500

    Added _init_ and license file to SRSConformality folder.  Converted Conformality.py to a menu plugin type.

--- plugins/SRSConformality/Conformality.py
+++ plugins/SRSConformality/Conformality.py
@@ -3,53 +3,71 @@
 # Conformality.py
 """dicompyler plugin that calculates congruence between selected structure and an isodose line."""
 
+import os.path, threading
 import wx
 from wx.xrc import XmlResource, XRCCTRL, XRCID
 from wx.lib.pubsub import Publisher as pub
-from matplotlib import _cntr as cntr
-from matplotlib import __version__ as mplversion
+#from matplotlib import _cntr as cntr
+#from matplotlib import __version__ as mplversion
 import matplotlib.nxutils as nx
 import numpy.ma as ma
 import numpy as np
-import guiutil, util
+from dicompyler import dvhdata
+#import guiutil, util
 
 def pluginProperties():
     """Properties of the plugin."""
 
     props = {}
-    props['name'] = 'Conformality'
+    props['name'] = 'SRS Conformality'
     props['description'] = "Calculate congruence between a selected structure and an isodose line."
     props['author'] = 'Landon Clark'
     props['version'] = 0.1
-    props['plugin_type'] = 'main'
+    props['plugin_type'] = 'menu'
     props['plugin_version'] = 1
-    props['min_dicom'] = ['images']
+    props['min_dicom'] = ['images', 'rtss', 'rtdose']
     props['recommended_dicom'] = ['images', 'rtss', 'rtdose']
 
     return props
 
-def pluginLoader(parent):
-    """Function to load the plugin."""
+class plugin:
+    """Calculates conformality of an isodose line to a structure."""
 
-    # Load the XRC file for our gui resources
-    res = XmlResource(util.GetBasePluginsPath('Conformality.xrc'))
+    def __init__(self, parent):
 
-    panelConformality = res.LoadPanel(parent, 'pluginConformality')
-    panelConformality.Init(res)
+        self.parent = parent
+        
+        # Set up pubsub
+        pub.subscribe(self.OnUpdatePatient, 'patient.updated.parsed_data')      
 
-    return panelConformality
+        # Load the XRC file for our gui resources
+        xrc = os.path.join(os.path.dirname(__file__), 'Conformality.xrc')
+        self.res = XmlResource(xrc)
 
-class plugin(wx.Panel):
+    def OnUpdatePatient(self, msg):
+        """Update and load the patient data."""
+
+        self.data = msg.data
+    
+    def pluginMenu(self, evt):
+        """Method called after the panel has been initialized."""
+
+        panelConformality = self.res.LoadDialog(self.parent, "ConformalityPanel")
+        panelConformality.SetTitle("SRS Conformality")
+        panelConformality.Init(self.data['structures'], self.data['dose'], self.data['plan'], self.data['dvhs'])
+        panelConformality.ShowModal() 
+        return panelConformality
+
+
+class ConformalityPanel(wx.Dialog):
     """Plugin to calculate congruence between selected structure and an isodose line."""
 
     def __init__(self):
-        pre = wx.PrePanel()
+        pre = wx.PreDialog()
         # the Create step is done by XRC.
         self.PostCreate(pre)
         
-
-    
-    def Init(self, res):
+    def Init(self, structures, dose, plan, dvhs):
         """Method called after the panel has been initialized."""
         
         # Initialize the Conformality selector controls
@@ -65,90 +83,31 @@ class plugin(wx.Panel):
         
         # Bind ui events to the proper methods
         wx.EVT_CHOICE(self, XRCID('choiceConformalityStructure'), self.OnStructureSelect)
-        wx.EVT_CHOICE(self, XRCID('choiceConformalityDose'), self.OnIsodoseCheck)
-        self.Bind(wx.EVT_WINDOW_DESTROY, self.OnDestroy)
+        wx.EVT_CHOICE(self, XRCID('choiceConformalityDose'), self.OnIsodoseSelect)
 
         # Initialize variables
+        self.structures = structures
+        self.dose = dose
+        self.rxdose = plan['rxdose']
+        self.dvhs = dvhs
         self.conformalitydata = {} 
         self.structureid = {}
+        self.dvhdata = {}
+        self.PopulateStructureChoices()
        
         # Setup toolbar controls
 
         # Set up preferences
-        
-        # Set up pubsub
-        pub.subscribe(self.OnUpdatePatient, 'patient.updated.raw_data')
-        pub.subscribe(self.OnUpdatePatient, 'patient.updated.parsed_data')
-        pub.subscribe(self.OnStructureCheck, 'colorcheckbox.checked.structure')
-        pub.subscribe(self.OnStructureUncheck, 'colorcheckbox.unchecked.structure')
-        
-    def OnUpdatePatient(self, msg):
-        """Update and load the patient data."""
-        
-        #print msg.data        
-        if msg.data.has_key('images'):
-            self.images = msg.data['images']
-        if msg.data.has_key('rxdose'):
-            self.rxdose = msg.data['rxdose']
-        if msg.data.has_key('rtdose'):
-            self.rtdose = msg.data['rtdose']
-        if msg.data.has_key('structures'):
-            self.structures = msg.data['structures']
-        if msg.data.has_key('dose'):    
-            self.dose = msg.data['dose']
-        self.EnableConformality(False)
     
-    def OnDestroy(self, evt):
-        """Unbind to all events before the plugin is destroyed."""
-        
-        pub.unsubscribe(self.OnUpdatePatient)
-        pub.unsubscribe(self.OnStructureCheck)
-        pub.unsubscribe(self.OnIsodoseCheck)
 
-    def OnStructureCheck(self, msg):
-        """Load a list of the currently checked structures."""
-
-        structure = msg.data
-
-        # Get the structure number
-        id = structure['data']['id']
-        structure['data']['color'] = structure['color'].Get()
-        self.EnableConformality(True)
-        i = self.choiceConformalityStructure.Append(structure['data']['name'])
-        self.choiceConformalityStructure.SetClientData(i, id)
-        # Select the first structure
-        self.OnStructureSelect()
-        if not len(structure):
-            self.EnableConformality(False)
-        
-    def OnStructureUncheck(self, msg):
-        """Remove the unchecked structures from the list."""
-
-        structure = msg.data
-
-        # Get the structure number
-        id = structure['data']['id']
-        # Remove the structure from the structure choice box
-        for n in range(self.choiceConformalityStructure.GetCount()):
-            if (id == self.choiceConformalityStructure.GetClientData(n)):
-                # Save if the currently selected item's position
-                currSelection = self.choiceConformalityStructure.GetSelection()
-                self.choiceConformalityStructure.Delete(n)
-                break
-
-        # If the currently selected item will be deleted,
-        # select the last item instead
-        if (n == currSelection):
-            if (self.choiceConformalityStructure.GetCount() >= 1):
-                self.OnStructureSelect()
-        # Disable the control if it is the last item
-        if (self.choiceConformalityStructure.GetCount() == 0):
-            self.EnableConformality(False)
-            self.OnStructureUnselect()
+    def PopulateStructureChoices(self):
+        """Load the plan structure list."""
+        self.choiceConformalityStructure.Clear()
+        for id, structure in self.structures.iteritems():
+            i = self.choiceConformalityStructure.Append(structure['name'])
+            self.choiceConformalityStructure.SetClientData(i, id)
 
     def OnStructureSelect(self, evt=None):
-        """Load the properties of the currently selected structure."""
-
         if (evt == None):
             self.choiceConformalityStructure.SetSelection(0)
             choiceItem = 0
@@ -156,22 +115,10 @@ class plugin(wx.Panel):
             choiceItem = evt.GetInt()
         # Load the structure id chosen from the choice control
         self.structureid = self.choiceConformalityStructure.GetClientData(choiceItem)
+        if self.choiceConformalityDose.GetSelection() > 0:
+            self.GetConformality(self.isodose)
 
-    def OnStructureUnselect(self):
-        """Clear the properties of the selected structure."""
-
-        self.lblConformalityIndex.SetLabel('-')
-        self.lblUnderdoseRatio.SetLabel('-')
-        self.lblOverdoseRatio.SetLabel('-')
-        self.lblCoverageVolume.SetLabel('-')
-
-    def EnableConformality(self, value):
-        """Enable or disable the constraint selector."""
-
-        self.choiceConformalityStructure.Enable(value)
-        self.choiceConformalityDose.Enable(value)
-
-    def OnIsodoseCheck(self, evt=None):
+    def OnIsodoseSelect(self, evt=None):
         """When the isodose list changes, update the panel."""
 
         if (evt == None):
@@ -180,34 +127,38 @@ class plugin(wx.Panel):
         else:
             choiceItem = evt.GetInt()
         isodoseID = self.choiceConformalityDose.GetSelection()
-        # Here "isodoseID == 0" is the '-' in the list, from conformality.xrc file
+        # Here "isodoseID == 0" is the '-' in the list, from the
+        # conformality.xrc file
         if isodoseID == 1:
-            isodose = 100
+            self.isodose = 100
         if isodoseID == 2:
-            isodose = 90
+            self.isodose = 90
         if isodoseID == 3:
-            isodose = 80
+            self.isodose = 80
         if isodoseID == 4:
-            isodose = 70            
+            self.isodose = 70            
         if isodoseID == 5:
-            isodose = 60        
+            self.isodose = 60        
         if isodoseID == 6:
-            isodose = 50     
-        dosedata = self.dose.GetDoseData()
+            self.isodose = 50     
+        self.GetConformality(self.isodose)
         
+    def GetConformality(self, isodose):
         #set thresholds for conformality
         lowerlimit = isodose * self.rxdose / 100   
         
-        structure = self.structures[self.structureid]
-        PITV, CV = CalculateCI(self, self.structures[self.structureid], lowerlimit)
-        TV = self.structures[self.structureid]['volume']
+        id = self.structureid
+        PITV, CV = CalculateCI(self, self.structures[id], lowerlimit)
+        self.dvhdata[id] = dvhdata.DVH(self.dvhs[id])
+        TV = dvhdata.CalculateVolume(self.structures[id])
         
-        self.lblTargetVolume.SetLabel(str(TV)[0:6])
-        self.lblCoverageVolume.SetLabel(str(CV)[0:6])
-        self.lblIsodoseVolume.SetLabel(str(PITV))
         self.lblConformalityIndex.SetLabel(str(CV*CV/(TV*PITV)*100)[0:5])
         self.lblUnderdoseRatio.SetLabel(str(CV/(TV)*100)[0:5])
         self.lblOverdoseRatio.SetLabel(str(CV/(PITV)*100)[0:5])
+        self.lblTargetVolume.SetLabel(str(TV)[0:6])
+        self.lblIsodoseVolume.SetLabel(str(PITV)[0:6])        
+        self.lblCoverageVolume.SetLabel(str(CV)[0:6])      
+    
         
 def CalculateCI(self, structure, lowerlimit):
     """From a selected structure and isodose line, return conformality index."""
@@ -232,10 +183,6 @@ def CalculateCI(self, structure, lowerlimit):
     dd = self.dose.GetDoseData()
     id = self.dose.GetImageData()
 
-    # Create an empty array of bins to store the histogram in cGy
-    # only if the structure has contour data
-    if len(sPlanes):
-        maxdose = int(dd['dosemax'] * dd['dosegridscaling'] * 100)
     PITV = 0   #Rx isodose volume in cc
     CV = 0    #coverage volume
 
